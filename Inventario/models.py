cd944d8b36cb49c4ec289f40f095d51b2055fae2
Justo <justojul23@gmail.com>
2019-04-29 21:25:20 -0600

    Nombres unicos

--- Inventario/models.py
+++ Inventario/models.py
@@ -3,26 +3,26 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Insumo(models.Model):
-    nombre = models.CharField(max_length=250)
+    nombre = models.CharField(max_length=250,unique= True)
     tipo_insumo  = models.ForeignKey('TipoInsumo')
     def __str__(self):
         return self.nombre
 
 class TipoInsumo(models.Model):
-    nombre = models.CharField(max_length=250)
+    nombre = models.CharField(max_length=250, unique= True)
     producto  = models.ForeignKey('Producto')
     def __str__(self):
         return self.nombre
 
 
 class Producto(models.Model):
-    nombre = models.CharField(max_length=250)
+    nombre = models.CharField(max_length=250, unique= True)
     precio = models.FloatField(validators=[MinValueValidator(0)])
     def __str__(self):
         return self.nombre
 
 class Promocion(models.Model):
-    nombre = models.CharField(max_length=250)
+    nombre = models.CharField(max_length=250, unique= True)
     precio = models.FloatField(validators=[MinValueValidator(0)])
     activa = models.BooleanField(default=True)
     def __str__(self):
